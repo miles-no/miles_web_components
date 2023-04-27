f62780e7ecea859432a4d6061faa0c1d67b02a65
christian skaiaa <skaihigh666@gmail.com>
2023-04-27 12:37:24 +0200

    initial checkin

---
+++ main.js
@@ -0,0 +1,146 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="components">
+  <!--
+    <miles-heart height="200"></miles-heart>
+    <miles-logo width="200"></miles-logo>
+    <miles-cloud height="50"></miles-cloud>
+    <miles-cup height="100"></miles-cup>
+    <miles-curve width="200"></miles-curve>
+    <miles-bulb height="50"></miles-bulb>
+    <miles-button-anchor href="https:www.vg.no">Trondheim</miles-button-anchor>
+    <miles-button-anchor>Data Science</miles-button-anchor>
+    <miles-profile-card name="Christian Skaiaa" description="Trondheim" image="https://akamai.vgc.no/users/schibsted/images/81801019.jpg?t[strip]=1&t[crop][x]=909&t[crop][y]=116&t[crop][width]=2952&t[crop][height]=2645&t[resize][width]=522&accessToken=fb625f22b82912106f78078ad09c76a11a6dacf2840dd2d1aef40e2a4463655c"></miles-profile-card>
+    <miles-info>Vi finner den perfekte konsulenten for deg og ditt prosjekt!</miles-info>
+    <miles-podcast-teaser></miles-podcast-teaser>
+
+    <miles-image-slider inview="3" autoplay="true">
+    <figure class="wp-block-image size-full"><img decoding="async" width="1200" height="800" data-id="8902" src="https://www.miles.no/newsite/wp-content/uploads/2023/01/U7A8645.jpg" alt="" class="wp-image-8902" srcset="https://www.miles.no/newsite/wp-content/uploads/2023/01/U7A8645.jpg 1200w, https://www.miles.no/newsite/wp-content/uploads/2023/01/U7A8645-825x550.jpg 825w, https://www.miles.no/newsite/wp-content/uploads/2023/01/U7A8645-768x512.jpg 768w, https://www.miles.no/newsite/wp-content/uploads/2023/01/U7A8645-1080x721.jpg 1080w, https://www.miles.no/newsite/wp-content/uploads/2023/01/U7A8645-385x258.jpg 385w" sizes="(max-width: 1200px) 100vw, 1200px"></figure>
+    <figure class="wp-block-image size-full"><img decoding="async" loading="lazy" width="1200" height="800" data-id="8861" src="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2400-@-Tony-Hall.jpg" alt="" class="wp-image-8861" srcset="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2400-@-Tony-Hall.jpg 1200w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2400-@-Tony-Hall-825x550.jpg 825w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2400-@-Tony-Hall-768x512.jpg 768w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2400-@-Tony-Hall-1080x721.jpg 1080w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2400-@-Tony-Hall-385x258.jpg 385w" sizes="(max-width: 1200px) 100vw, 1200px"></figure>
+    <figure class="wp-block-image size-full"><img decoding="async" loading="lazy" width="1200" height="800" data-id="8858" src="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2595-@-Tony-Hall.jpg" alt="" class="wp-image-8858" srcset="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2595-@-Tony-Hall.jpg 1200w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2595-@-Tony-Hall-825x550.jpg 825w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2595-@-Tony-Hall-768x512.jpg 768w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2595-@-Tony-Hall-1080x721.jpg 1080w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2595-@-Tony-Hall-385x258.jpg 385w" sizes="(max-width: 1200px) 100vw, 1200px"></figure>
+    <figure class="wp-block-image size-full"><img decoding="async" loading="lazy" width="1200" height="800" data-id="8854" src="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2336-@-Tony-Hall.jpg" alt="" class="wp-image-8854" srcset="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2336-@-Tony-Hall.jpg 1200w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-825x550.jpg 825w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-768x512.jpg 768w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-1080x721.jpg 1080w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-385x258.jpg 385w" sizes="(max-width: 1200px) 100vw, 1200px"></figure>
+  </miles-image-slider>
+
+  <miles-overlap-block>
+  <div id="" class="custom-block-feature-block miles-overlap-block">
+
+
+  <div class="feature-wrapper">
+          <div class="feature-content">
+                                  <h2 class="feature-title">
+                    Kompetanse i særklasse						       </h2>
+                                                     <p class="feature-text">
+                    Miles er et IT-konsulentselskap der gleden over faget og hverandre er de viktigste ledestjernene. Kjernekompetansen vår ligger i utvikling, design, prosjetledelse og rådgivning.						       </p>
+                                  
+                
+                
+                         
+                                                       
+      </div>
+  </div>       			
+</div>
+
+
+</miles-overlap-block>  
+
+
+
+
+ <miles-image-slider inview="3" autoplay="true">
+ <figure class="is-layout-flex wp-block-gallery-1 wp-block-gallery has-nested-images columns-default is-cropped miles-image-slider">
+<figure class="wp-block-image size-large"><img decoding="async" loading="lazy" width="825" height="550" data-id="8854" src="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-825x550.jpg" alt="" class="wp-image-8854" srcset="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-825x550.jpg 825w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-768x512.jpg 768w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-1080x721.jpg 1080w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-385x258.jpg 385w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2336-@-Tony-Hall.jpg 1200w" sizes="(max-width: 825px) 100vw, 825px"></figure>
+<figure class="wp-block-image size-large"><img decoding="async" loading="lazy" width="825" height="550" data-id="8858" src="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2595-@-Tony-Hall-825x550.jpg" alt="" class="wp-image-8858" srcset="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2595-@-Tony-Hall-825x550.jpg 825w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2595-@-Tony-Hall-768x512.jpg 768w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2595-@-Tony-Hall-1080x721.jpg 1080w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2595-@-Tony-Hall-385x258.jpg 385w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2595-@-Tony-Hall.jpg 1200w" sizes="(max-width: 825px) 100vw, 825px"></figure>
+<figure class="wp-block-image size-large"><img decoding="async" loading="lazy" width="825" height="550" data-id="8863" src="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2711-@-Tony-Hall-825x550.jpg" alt="" class="wp-image-8863" srcset="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2711-@-Tony-Hall-825x550.jpg 825w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2711-@-Tony-Hall-768x512.jpg 768w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2711-@-Tony-Hall-1080x721.jpg 1080w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2711-@-Tony-Hall-385x258.jpg 385w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2711-@-Tony-Hall.jpg 1200w" sizes="(max-width: 825px) 100vw, 825px"></figure>
+<figure class="wp-block-image size-large"><img decoding="async" loading="lazy" width="825" height="550" data-id="8861" src="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2400-@-Tony-Hall-825x550.jpg" alt="" class="wp-image-8861" srcset="https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2400-@-Tony-Hall-825x550.jpg 825w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2400-@-Tony-Hall-768x512.jpg 768w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2400-@-Tony-Hall-1080x721.jpg 1080w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2400-@-Tony-Hall-385x258.jpg 385w, https://www.miles.no/newsite/wp-content/uploads/2023/01/AFH2400-@-Tony-Hall.jpg 1200w" sizes="(max-width: 825px) 100vw, 825px"></figure>
+<figure class="wp-block-image size-large"><img decoding="async" loading="lazy" width="823" height="550" data-id="8730" src="https://www.miles.no/newsite/wp-content/uploads/2022/12/DSC05637-823x550.jpg" alt="" class="wp-image-8730" srcset="https://www.miles.no/newsite/wp-content/uploads/2022/12/DSC05637-823x550.jpg 823w, https://www.miles.no/newsite/wp-content/uploads/2022/12/DSC05637-768x513.jpg 768w, https://www.miles.no/newsite/wp-content/uploads/2022/12/DSC05637-1080x721.jpg 1080w, https://www.miles.no/newsite/wp-content/uploads/2022/12/DSC05637-385x258.jpg 385w, https://www.miles.no/newsite/wp-content/uploads/2022/12/DSC05637.jpg 1200w" sizes="(max-width: 823px) 100vw, 823px"></figure>
+<figure class="wp-block-image size-large"><img decoding="async" loading="lazy" width="825" height="550" data-id="8707" src="https://www.miles.no/newsite/wp-content/uploads/2022/11/0U7A7621-825x550.jpg" alt="" class="wp-image-8707" srcset="https://www.miles.no/newsite/wp-content/uploads/2022/11/0U7A7621-825x550.jpg 825w, https://www.miles.no/newsite/wp-content/uploads/2022/11/0U7A7621-768x512.jpg 768w, https://www.miles.no/newsite/wp-content/uploads/2022/11/0U7A7621-1080x721.jpg 1080w, https://www.miles.no/newsite/wp-content/uploads/2022/11/0U7A7621-385x258.jpg 385w, https://www.miles.no/newsite/wp-content/uploads/2022/11/0U7A7621.jpg 1200w" sizes="(max-width: 825px) 100vw, 825px"></figure>
+</figure>
+ </miles-image-slider>
+
+ <miles-fagblogg-teaser>
+ <ul class="wp-block-latest-posts__list has-dates has-author miles-fagblogg-teaser wp-block-latest-posts"><li><div class="wp-block-latest-posts__featured-image aligncenter"><img loading="lazy" src="https://www.miles.no/newsite/wp-content/uploads/2023/01/Kopi-av-Alexander-Istad-Funch-260x260.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="" decoding="async" style="max-width:260px;max-height:260px;" srcset="https://www.miles.no/newsite/wp-content/uploads/2023/01/Kopi-av-Alexander-Istad-Funch-260x260.jpg 260w, https://www.miles.no/newsite/wp-content/uploads/2023/01/Kopi-av-Alexander-Istad-Funch-550x550.jpg 550w, https://www.miles.no/newsite/wp-content/uploads/2023/01/Kopi-av-Alexander-Istad-Funch-768x768.jpg 768w, https://www.miles.no/newsite/wp-content/uploads/2023/01/Kopi-av-Alexander-Istad-Funch.jpg 1200w" sizes="(max-width: 260px) 100vw, 260px"></div><a class="wp-block-latest-posts__post-title" href="https://www.miles.no/newsite/mangel-pa-tillit-kan-kjore-it-utviklingen-i-groften/">Mangel på tillit kan kjøre IT-utviklingen i grøften</a><div class="wp-block-latest-posts__post-author">av Ida Severinsen</div><time datetime="2023-01-23T11:23:47+00:00" class="wp-block-latest-posts__post-date">23/01/2023</time></li>
+ <li><div class="wp-block-latest-posts__featured-image aligncenter"><img src="https://www.miles.no/newsite/wp-content/uploads/2023/01/0U7A7621-260x260.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="" decoding="async" loading="lazy" style="max-width:260px;max-height:260px;"></div><a class="wp-block-latest-posts__post-title" href="https://www.miles.no/newsite/fagdag-med-fokus-pa-varme/">Fagdag med fokus på varme</a><div class="wp-block-latest-posts__post-author">av Ida Severinsen</div><time datetime="2023-01-02T14:01:28+00:00" class="wp-block-latest-posts__post-date">02/01/2023</time></li>
+ <li><div class="wp-block-latest-posts__featured-image aligncenter"><img src="https://www.miles.no/newsite/wp-content/uploads/2022/12/GPTempDownload-1-260x260.jpg" class="attachment-thumbnail size-thumbnail wp-post-image" alt="" decoding="async" loading="lazy" style="max-width:260px;max-height:260px;"></div><a class="wp-block-latest-posts__post-title" href="https://www.miles.no/newsite/qcon-2022/">QCon 2022</a><div class="wp-block-latest-posts__post-author">av Ida Severinsen</div><time datetime="2022-12-06T12:02:30+00:00" class="wp-block-latest-posts__post-date">06/12/2022</time></li>
+ </ul>
+ </miles-fagblogg-teaser>
+-->
+<miles-office-banner>
+<div class="is-layout-constrained wp-block-group miles-office-banner"><div class="wp-block-group__inner-container">
+<div id="" class="custom-block-feature-block">
+
+
+		<div class="feature-wrapper">
+							<figure class="feature-image">
+                    <img width="900" height="721" src="https://www.miles.no/newsite/wp-content/uploads/2022/06/280387772_1197150211029978_3320966336713086871_n-900x721.jpeg" class="attachment-feature-image size-feature-image" alt="" decoding="async" loading="lazy">                </figure>
+						<div class="feature-content">
+						     						       <h2 class="feature-title">
+						          Stavanger						       </h2>
+						      						      						      
+						      
+						      
+    	                     
+    	                   						     					     
+				</div>
+		</div>       			
+</div>
+
+
+
+<div class="is-layout-constrained wp-block-group miles-office-menu"><div class="wp-block-group__inner-container">
+<h2 id="h-miles-oslo-as">Miles Oslo AS</h2>
+
+
+
+<ul>
+<li>Bislettgata 4, 0170 Oslo</li>
+
+
+
+<li>oslo@miles.no</li>
+
+
+
+<li>+47 95144636</li>
+
+
+
+<li>Organisasjonsnummer:</li>
+
+
+
+<li>888 884 122 MVA</li>
+</ul>
+
+
+
+<div class="is-layout-constrained wp-block-group miles-banner-card"><div class="wp-block-group__inner-container">
+<ul>
+<li>Bjørn Olav Semundseth</li>
+
+
+
+<li>Daglig tjener Miles Oslo</li>
+
+
+
+<li>Telefon: 906 52 950</li>
+
+
+
+<li>Epost: bjorn@miles.no</li>
+</ul>
+
+
+
+<figure class="wp-block-image size-large"><img decoding="async" width="978" height="550" src="https://www.miles.no/newsite/wp-content/uploads/2022/11/nick-night-tV2TOBbxyF0-unsplash-1-978x550.jpg" alt="" class="wp-image-8552" srcset="https://www.miles.no/newsite/wp-content/uploads/2022/11/nick-night-tV2TOBbxyF0-unsplash-1-978x550.jpg 978w, https://www.miles.no/newsite/wp-content/uploads/2022/11/nick-night-tV2TOBbxyF0-unsplash-1-768x432.jpg 768w, https://www.miles.no/newsite/wp-content/uploads/2022/11/nick-night-tV2TOBbxyF0-unsplash-1.jpg 1200w" sizes="(max-width: 978px) 100vw, 978px"></figure>
+</div></div>
+</div></div>
+</div></div>
+</miles-office-banner>
+
+  </div>
+
+
+`
+
