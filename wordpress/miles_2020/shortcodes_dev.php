8fbbec90fed9b28f4e31416a896000a8f86eca32
christian skaiaa <skaihigh666@gmail.com>
2023-04-28 12:43:04 +0200

    fix wrong function name

--- wordpress/miles_2020/shortcodes_dev.php
+++ wordpress/miles_2020/shortcodes_dev.php
@@ -4,11 +4,11 @@
 
 function register_shortcodes_dev(): void
 {
-    add_shortcode('miles_info_block', 'get_consultant_group_shortcode');
+    add_shortcode('miles_info_block', 'miles_info_block_shortcode');
 }
 
 
-function miles_info_block( $atts, $content = null ) {
+function miles_info_block_shortcode( $atts, $content = null ) {
     $heading = $atts['heading'] ?? 'default heading';
     if ( $content == null ) {
         $content = 'default content';
