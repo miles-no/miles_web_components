fbce123cf3ee27a1da8004f1806e82c37b8526f8
christian skaiaa <skaihigh666@gmail.com>
2023-06-09 08:58:52 +0200

    custom player finnished v01

--- src/icons/index.js
+++ src/icons/index.js
@@ -506,10 +506,10 @@ if (!customElements.get(MilesEmailName)) {
 const templatePlay = document.createElement('template');
 templatePlay.innerHTML = `
       <style></style>
-    <svg width="92" height="92" viewBox="0 0 92 92" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="46" cy="46" r="46" fill="#EB4645"></circle>
-                <path d="M32 27V65L70 46L32 27Z" fill="#FCF8F3"></path>
-              </svg>    
+      <svg xmlns="http://www.w3.org/2000/svg" height="64" viewBox="0 -960 960 960" width="64">
+        <path fill="#EB4645" d="m418-332 202-129q11-7 11-19t-11-19L418-628q-11-8-23-1.5T383-609v258q0 14 12 20.5t23-1.5Zm62 252q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z"/>
+      </svg>
+
   `;
 
 class MilesPlay extends HTMLElement {
@@ -533,10 +533,9 @@ if (!customElements.get(MilesPlayName)) {
 const templatePause = document.createElement('template');
 templatePause.innerHTML = `
       <style></style>
- <svg width="92" height="92" viewBox="0 0 92 92" fill="none" xmlns="http://www.w3.org/2000/svg">
- <circle cx="46" cy="46" r="46" fill="#EB4645"></circle>
-  <path d="M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26" fill="#FCF8F3"></path>
-</svg>  
+<svg xmlns="http://www.w3.org/2000/svg" height="64" viewBox="0 -960 960 960" width="64">
+  <path fill="#EB4645" d="M400.175-320q12.825 0 21.325-8.625T430-350v-260q0-12.75-8.675-21.375-8.676-8.625-21.5-8.625-12.825 0-21.325 8.625T370-610v260q0 12.75 8.675 21.375 8.676 8.625 21.5 8.625Zm160 0q12.825 0 21.325-8.625T590-350v-260q0-12.75-8.675-21.375-8.676-8.625-21.5-8.625-12.825 0-21.325 8.625T530-610v260q0 12.75 8.675 21.375 8.676 8.625 21.5 8.625ZM480.266-80q-82.734 0-155.5-31.5t-127.266-86q-54.5-54.5-86-127.341Q80-397.681 80-480.5q0-82.819 31.5-155.659Q143-709 197.5-763t127.341-85.5Q397.681-880 480.5-880q82.819 0 155.659 31.5Q709-817 763-763t85.5 127Q880-563 880-480.266q0 82.734-31.5 155.5T763-197.684q-54 54.316-127 86Q563-80 480.266-80Zm.234-60Q622-140 721-239.5t99-241Q820-622 721.188-721 622.375-820 480-820q-141 0-240.5 98.812Q140-622.375 140-480q0 141 99.5 240.5t241 99.5Zm-.5-340Z"/>
+</svg>   
   `;
 
 class MilesPause extends HTMLElement {
