d8f2d7b836e07e60e44b55ea451da20b9a2430fe
Simon Degen <simondegen99@gmail.com>
2023-06-13 13:28:33 +0200

    added site menu

---
+++ src/stories/components/site-menu.stories.js
@@ -0,0 +1,8 @@
+export default {
+  title: 'Miles/Components',
+  tags: ['autodocs'],
+  render: args => `<miles-site-menu></miles-site-menu>`,
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/web-components/writing-stories/args
+export const SiteMenu = {};
