80bc23ad811a8c9ab62e3fe56df8e3f54d8f843a
Lukas Juodikis <lukas.juodikis@komponent.no>
2023-06-16 15:46:35 +0300

    Fix office-banner component

--- src/office-banner/index.js
+++ src/office-banner/index.js
@@ -7,79 +7,76 @@ import cssVariables from '../styles/variables.css?inline';
 
 const MilesOfficeBannerTemplate = document.createElement('template');
 MilesOfficeBannerTemplate.innerHTML = `
-  <style>
-	${styles}\n
-  ${cssVariables}
-  </style>
-  <div id="banner">
-    <slot name="bannerimage"></slot>
-    <div id="menu">
-      <div id="people"><slot></slot></div>
-    </div>
-    <div id="triggerBg">
-      <div id="triggerEl"><slot name="bannerheading"></slot><miles-arrow class="open" ></miles-arrow></div>
-    </div>
-    <div id="content">
-      
-    </div>
-  </div>
+	<style>
+		${styles}
+		${cssVariables}
+	</style>
+	<div id="banner" class="office-banner">
+		<div class="office-banner__content">
+		    <div class="office-banner__trigger js-banner-trigger">
+		        <h2 class="office-banner__title">Oslo</h2>
+		        <miles-arrow class="office-banner__trigger-arrow"></miles-arrow>
+		    </div>
+		    <div class="anim">
+			    <div class="office-banner__details">
+			        <div class="office-banner__office">
+			            <slot name="office"></slot>
+			        </div>
+			        <div class="office-banner__people">
+			            <slot></slot>
+			        </div>
+			    </div>
+		    </div>
+        </div>
+		<div class="office-banner__background" id="bannerImage"></div>
+	</div>
 `;
 
 class MilesOfficeBanner extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-    this.shadowRoot.append(MilesOfficeBannerTemplate.content.cloneNode(true));
-    this.banner = this.shadowRoot.querySelector('#banner');
-    this.menu = this.shadowRoot.querySelector('#menu');
-    this.people = this.shadowRoot.querySelector('#people');
-    this.triggerEl = this.shadowRoot.querySelector('#triggerEl');
-    this.triggerBg = this.shadowRoot.querySelector('#triggerBg');
-    this.header = null;
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+		this.shadowRoot.append(MilesOfficeBannerTemplate.content.cloneNode(true));
+		this.banner = this.shadowRoot.querySelector('#banner');
+		this.triggerEl = this.shadowRoot.querySelector('.js-banner-trigger');
+
+
+		this.backgroundEl = this.shadowRoot.querySelector('.office-banner__background');
+		this.titleEl = this.shadowRoot.querySelector('.office-banner__title');
+		this.header = null;
+	}
+
+  static get observedAttributes() {
+    return ['background', 'city'];
   }
 
-  connectedCallback() {
-    const rects = this.banner.getBoundingClientRect();
-    if (this.triggerEl && rects.width > 769) {
-      this.triggerEl.setAttribute('style', 'cursor: pointer; ');
-      this.triggerBg.addEventListener('click', this.toggleMenu);
-    } else {
-      this.triggerEl.querySelector('miles-arrow').classList.add('open');
-      this.menu.classList.add('open');
-      this.banner.classList.add('open');
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'background') {
+      this.backgroundEl.style.backgroundImage = `url(${newValue})`;
+    }
+
+    if (name === 'city') {
+      this.titleEl.textContent = newValue;
     }
   }
 
-  disconnectedCallback() {
-    this.triggerBg.removeEventListener('click', this.toggleMenu);
-  }
+	connectedCallback() {
+		this.triggerEl.addEventListener('click', this.toggleMenu);
+	}
 
-  toggleMenu = () => {
-    const cards = this.menu.querySelectorAll('miles-business-card');
-    const rects = this.banner.getBoundingClientRect();
-    this.menu.classList.toggle('open');
-    this.banner.classList.toggle('open');
-    if (this.menu.classList.contains('open')) {
-      this.triggerEl.querySelector('miles-arrow').classList.remove('open');
-      if (rects.width < 769) {
-        this.banner.setAttribute(
-          'style',
-          `height: calc(calc(${cards.length} * var(--headerHeight)) + 300px);`
-        );
-      }
-    } else {
-      this.banner.removeAttribute('style');
+	disconnectedCallback() {
+		this.triggerBg.removeEventListener('click', this.toggleMenu);
+	}
 
-      this.triggerEl.querySelector('miles-arrow').classList.add('open');
-      this.banner.classList.toggle('open');
-    }
-  };
+	toggleMenu = () => {
+		this.banner.classList.toggle('office-banner--open');
+	};
 }
 
 const MilesOfficeBannerName = 'miles-office-banner';
 
 if (!customElements.get(MilesOfficeBannerName)) {
-  customElements.define(MilesOfficeBannerName, MilesOfficeBanner);
+	customElements.define(MilesOfficeBannerName, MilesOfficeBanner);
 }
 
 export default MilesOfficeBanner;
