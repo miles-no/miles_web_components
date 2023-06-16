c1823bf4e2100deb5a2cd6e45d050c74e6b56bf1
christian skaiaa <skaihigh666@gmail.com>
2023-06-16 12:36:49 +0200

    more slot content

--- src/stories/components/image-slider.stories.js
+++ src/stories/components/image-slider.stories.js
@@ -4,13 +4,23 @@ export default {
   title: 'Miles/Components',
   tags: ['autodocs'],
   render: args =>
-    `<miles-image-slider  ${propsToAttrs(args)}></miles-image-slider>`,
+    `<miles-image-slider  ${propsToAttrs(args)}>
+      <figure class="is-layout-flex wp-block-gallery-1 wp-block-gallery has-nested-images columns-default is-cropped miles-image-slider">
+        <figure class="wp-block-image size-large tv2"></figure>
+        <figure class="wp-block-image size-large equinor"></figure>
+        <figure class="wp-block-image size-large fjordkraft"></figure>
+        <figure class="wp-block-image size-large cutters"></figure>
+        <figure class="wp-block-image size-large tv2"></figure>
+        <figure class="wp-block-image size-large scaleaq"></figure>
+        <figure class="wp-block-image size-large"></figure>
+      </figure>
+    </miles-image-slider>`,
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/web-components/writing-stories/args
 export const ImageSlider = {
   args: {
-    inview: 'true',
+    inview: '3',
     autoplay: 'true',
   },
 };
