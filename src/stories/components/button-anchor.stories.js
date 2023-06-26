8f3550153afaf2b6c71fd22924f351a7ad41a1e5
Lukas Juodikis <lukas.juodikis@komponent.no>
2023-06-26 15:01:01 +0300

    Fix button-anchor story

--- src/stories/components/button-anchor.stories.js
+++ src/stories/components/button-anchor.stories.js
@@ -1,5 +1,3 @@
-import { propsToAttrs } from '../storybook.utils.js';
-
 export default {
   title: 'Miles/Components',
   tags: ['autodocs'],
@@ -7,20 +5,20 @@ export default {
     selected: {
       control: 'boolean',
       description: 'select or unselect button',
-    },
   },
   render: args =>
-    `<miles-button-anchor  ${propsToAttrs(
-      args
-    )}>Miles Button Anchor</miles-button-anchor>`,
+    `<miles-button-anchor href="${args.href}" color="${args.color}" selected="${args.selected}" count="${args.count}">
+        ${args.children}
+    </miles-button-anchor>`,
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/web-components/writing-stories/args
 export const ButtonAnchor = {
   args: {
+    children: 'Button text',
     href: 'https://archive.org/embed/NeverGonnaGiveYouUp',
     color: '#3F1221',
     selected: 'false',
-    count: 15,
+    count: 0,
   },
 };
