625f91ab3e84dde656eec3a1db72954dafcb1b21
Lukas Juodikis <lukas.juodikis@komponent.no>
2023-06-29 09:35:47 +0300

    Remove html as prop from stories

--- src/image-block/image-block.stories.js
+++ src/image-block/image-block.stories.js
@@ -18,7 +18,9 @@ export default {
 	},
 	render: args => `<div style="width: 100%;max-width: 1440px;">
 		<miles-image-block background="${args.background}" color="${args.color}" reverse="${args.reverse}" image="${args.image}" button="${args.button}" href="${args.href}" alt="${args.alt}">
-			${args.children}
+			<h2>Lorem ipsum</h2>
+			<p>Donec sagittis tellus mi, sed consequat nisi luctus et. Donec porttitor dignissim leo in euismod. Pellentesque egestas volutpat tristique. Praesent dolor arcu, fermentum vel scelerisque eu, efficitur ac ex. Proin lacus est, venenatis id feugiat ut, molestie a neque.</p>
+			<p>Maecenas at ipsum sollicitudin, egestas sapien eu, auctor erat. Morbi id rhoncus ante. In id felis nisl.</p>
         </miles-image-block>
     </div>`,
 };
@@ -26,8 +28,6 @@ export default {
 
 export const ImageBlock = {
 	args: {
-		// eslint-disable-next-line xss/no-mixed-html
-		children: '<h2>Lorem ipsum</h2><p> Donec sagittis tellus mi, sed consequat nisi luctus et. Donec porttitor dignissim leo in euismod. Pellentesque egestas volutpat tristique. Praesent dolor arcu, fermentum vel scelerisque eu, efficitur ac ex. Proin lacus est, venenatis id feugiat ut, molestie a neque.</p><p>Maecenas at ipsum sollicitudin, egestas sapien eu, auctor erat. Morbi id rhoncus ante. In id felis nisl.</p>',
 		image: 'https://loremflickr.com/540/420/dog',
 		alt: '',
 		reverse: false,
