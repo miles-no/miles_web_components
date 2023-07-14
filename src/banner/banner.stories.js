bc329181eb87a6fec136b7c460b9b112371e406f
Lukas Juodikis <lukas.juodikis@komponent.no>
2023-07-14 11:19:30 +0300

    Fix banner (inner HTML support, sizing)

--- src/banner/banner.stories.js
+++ src/banner/banner.stories.js
@@ -3,7 +3,9 @@ import { propsToAttrs } from '../storybook.utils.js';
 export default {
   title: 'Miles/Components',
   tags: ['autodocs'],
-  render: args => `<miles-banner  ${propsToAttrs(args)}></miles-banner>`,
+  render: args => `<miles-banner image="${args.image}" title="${args.title}" variant="${args.variant}" reverse="${args.reverse}">
+    ${args.text}
+    </miles-banner>`,
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/web-components/writing-stories/args
@@ -12,8 +14,8 @@ export const Banner = {
     image:
       'https://www.miles.no/wp-content/uploads/2021/11/Lily-258.jpg',
     title: 'Welcome to our website!',
-    slogan: 'Discover our latest products',
+    text: '<p>Now supporting <strong>bold</strong>, <em>italic</em>, <span style="background:#004047;color:#fff;">colours</span> and other HTML tags</p>',
     variant: 'split',
-    reverse: true,
+    reverse: false,
   },
 };
