dab42273778070d02f3be5afc2aff815d73a5fbd
Knut Hufthammer <knut.hufthammer@miles.no>
2023-10-06 08:46:33 +0200

    #93 added story to banner

--- src/banner/banner.stories.js
+++ src/banner/banner.stories.js
@@ -20,3 +20,20 @@ export const Banner = {
     icon: 'miles-curve',
   },
 };
+
+export const BannerWithContentOnRightSide = {
+  args: {
+    title: 'Welcome to our website!',
+    variant: 'split',
+    reverse: false,
+    icon: 'miles-curve',
+  },
+  render: args => {
+    return `<miles-banner ${propsToAttrs(args)}>
+      <p>Now supporting <strong>bold</strong>, <em>italic</em>, <span style="background:#004047;color:#fff;">colours</span> and other HTML tags</p>
+      <div slot="right__banner__content" style="background-color: #450d21; height: 100%; display: flex; align-items: center; justify-content: center;">
+        <p style="color: white;">I'm on the right side</p>
+      </div>
+    </miles-banner>`
+  }
+};
