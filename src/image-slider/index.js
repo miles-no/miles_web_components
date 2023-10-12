bf94add233bdef021ed1bd95147a91ac66971fba
Thomas Heritier <thomas.heritier@miles.no>
2023-10-12 14:36:36 +0200

    more stuff

--- src/image-slider/index.js
+++ src/image-slider/index.js
@@ -7,9 +7,6 @@ import { Transition } from './Transition';
 /**
  * Miles Image Slider
  */
-
-const BASE_URL = 'https://miles.no/newsite/';
-
 const ImageSliderTemplate = document.createElement('template');
 ImageSliderTemplate.innerHTML = `
 <style>
@@ -18,77 +15,21 @@ ${styles}\n
 ${cssVariables}
 </style>
 <section class="splide" aria-label="Splide Basic HTML Example">
-  <div class="splide__track">
-    <ul class="splide__list">
-      <li class="splide__slide">
-        <div class="splide__slide__container">
-          <figure data-slide-image="0">
-            <div class="overlay tv2"></div>
-            <img decoding="async" loading="lazy" width="825" height="550"
-              src="${BASE_URL}wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-825x550.jpg" alt="" class="wp-image-8854"
-              srcset="
-                    ${BASE_URL}wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-825x550.jpg   825w,
-                    ${BASE_URL}wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-768x512.jpg   768w,
-                    ${BASE_URL}wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-1080x721.jpg 1080w,
-                    ${BASE_URL}wp-content/uploads/2023/01/AFH2336-@-Tony-Hall-385x258.jpg   385w,
-                    ${BASE_URL}wp-content/uploads/2023/01/AFH2336-@-Tony-Hall.jpg          1200w
-                  " sizes="(max-width: 825px) 100vw, 825px" draggable="false" />
-          </figure>
-        </div>
-      </li>
-      <li class="splide__slide">
-        <div class="splide__slide__container">
-          <figure data-slide-image="0">
-            <div class="overlay tv2"></div>
-            <img decoding="async" loading="lazy" width="825" height="550"
-              src="${BASE_URL}wp-content/uploads/2022/11/0U7A7621-825x550.jpg" alt="" class="wp-image-8707" srcset="
-                ${BASE_URL}wp-content/uploads/2022/11/0U7A7621-825x550.jpg   825w,
-                ${BASE_URL}wp-content/uploads/2022/11/0U7A7621-768x512.jpg   768w,
-                ${BASE_URL}wp-content/uploads/2022/11/0U7A7621-1080x721.jpg 1080w,
-                ${BASE_URL}wp-content/uploads/2022/11/0U7A7621-385x258.jpg   385w,
-                ${BASE_URL}wp-content/uploads/2022/11/0U7A7621.jpg          1200w
-              " sizes="(max-width: 825px) 100vw, 825px" draggable="false" />
-          </figure>
-        </div>
-      </li>
-      <li class="splide__slide">
-        <div class="splide__slide__container">
-          <figure data-slide-image="0">
-            <div class="overlay tv2"></div>
-             <img decoding="async" loading="lazy" width="508" height="550"
-              src="${BASE_URL}wp-content/uploads/2023/05/Trondheim-3-508x550.jpg" alt="" class="wp-image-9825" srcset="
-                ${BASE_URL}wp-content/uploads/2023/05/Trondheim-3-508x550.jpg  508w,
-                ${BASE_URL}wp-content/uploads/2023/05/Trondheim-3-768x831.jpg  768w,
-                ${BASE_URL}wp-content/uploads/2023/05/Trondheim-3.jpg         1109w
-              " sizes="(max-width: 508px) 100vw, 508px" draggable="false" />
-          </figure>
-        </div>
-      </li>
-      <li class="splide__slide">
-        <div class="splide__slide__container">
-          <figure data-slide-image="0">
-            <div class="overlay tv2"></div>
-              <img
-                decoding="async"
-                loading="lazy"
-                width="463"
-                height="550"
-                src="${BASE_URL}wp-content/uploads/2023/05/Stavanger-463x550.jpg"
-                alt=""
-                class="wp-image-9826"
-                srcset="
-                  ${BASE_URL}wp-content/uploads/2023/05/Stavanger-463x550.jpg  463w,
-                  ${BASE_URL}wp-content/uploads/2023/05/Stavanger-768x912.jpg  768w,
-                  ${BASE_URL}wp-content/uploads/2023/05/Stavanger.jpg         1010w
-                "
-                sizes="(max-width: 463px) 100vw, 463px"
-                draggable="false" />
-          </figure>
-        </div>
-      </li>
-    </ul>
+  <div class="splide__slider">
+    <div class="splide__track">
+      <ul class="splide__list">
+      </ul>
+    </div>
   </div>
 </section>
+`;
+
+// Reuse the same slide element for all slides
+const ImageSliderSlide = document.createElement('li');
+ImageSliderSlide.className = 'splide__slide';
+ImageSliderSlide.innerHTML = `
+  <div class="splide__slide__container">
+  </div>
 `;
 
 class MilesImageSlider extends HTMLElement {
@@ -96,19 +37,51 @@ class MilesImageSlider extends HTMLElement {
     super();
     const shadow = this.attachShadow({ mode: 'open' });
     shadow.appendChild(ImageSliderTemplate.content.cloneNode(true));
-    this.splide = shadow.querySelector('.splide');
+
+    /* this.logos = [
+      'fjordkraft',
+      'tv2',
+      'cutters',
+      'scaleaq',
+      'telenor',
+      'ruter',
+      'dnb',
+      'ifforsikring',
+      'politiet',
+    ]; */
+
+    this.createSlides();
+    this.initializeSplide();
   }
 
-  connectedCallback() {
-    new Splide(this.splide, {
+  get figures() {
+    const figures = this.querySelectorAll('figure');
+    return [...figures];
+  }
+
+  createSlides() {
+    const ul = this.shadowRoot.querySelector('ul');
+    this.figures.forEach(figure => {
+      const slide = ul.appendChild(ImageSliderSlide.cloneNode(true));
+      const container = slide.querySelector('.splide__slide__container');
+      container.appendChild(figure);
+    });
+  }
+
+  initializeSplide() {
+    const options = {
       type: 'loop',
       focus: 'center',
       drag: 'free',
       snap: true,
       fixedWidth: '468px',
       fixedHeight: '468px',
-      autoplay: 'play',
-    }).mount({}, Transition);
+    };
+
+    new Splide(this.shadowRoot.querySelector('.splide'), options).mount(
+      {},
+      Transition
+    );
   }
 }
 
