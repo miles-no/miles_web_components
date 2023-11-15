2b6dba95f17564ac6b78cacab370ac75a7ea7c1d
Thomas <thomas.heritier@gmail.com>
2023-11-15 11:24:33 +0100

    change overlap block design (#115)
    * change overlap block design

--- src/overlap-block/overlap-block.stories.js
+++ src/overlap-block/overlap-block.stories.js
@@ -3,22 +3,24 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 export default {
   title: 'Miles/Components',
   tags: ['autodocs'],
-  render: args =>
-    `<miles-overlap-block ${propsToAttrs(args)}>
-      <div id="" class="custom-block-feature-block miles-overlap-block">
-    		<div class="feature-wrapper">
-						<div class="feature-content">
-				       <h2 class="feature-title">Kompetanse i særklasse</h2>
-				       <p class="feature-text">
-				          Miles er et IT-konsulentselskap der gleden over faget og hverandre er de viktigste ledestjernene. Kjernekompetansen vår ligger i utvikling, design, prosjetledelse og rådgivning.
-              </p>
-              <div class="link_wrapper">
-				        <a href="${BASE_URL}}dette-kan-vi-i-miles/" class="link">Les mer om vår kompetanse </a>
-              </div>
-				</div>
-		</div>       			
-</div>
-    </miles-overlap-block`,
+  render: args => `
+<miles-overlap-block ${propsToAttrs(args)}>
+  <div id="" class="custom-block-feature-block miles-overlap-block">
+    <div class="feature-wrapper">
+      <div class="feature-content">
+        <h2 class="feature-title">Kompetanse i særklasse</h2>
+        <p class="feature-text">
+          Miles er et IT-konsulentselskap der gleden over faget og hverandre er de viktigste ledestjernene.
+          Kjernekompetansen vår ligger i utvikling, design, prosjetledelse og rådgivning.
+        </p>
+        <div class="link_wrapper">
+          <a href="${BASE_URL}}dette-kan-vi-i-miles/" class="link">Les mer om vår kompetanse </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</miles-overlap-block>
+`,
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/web-components/writing-stories/args
